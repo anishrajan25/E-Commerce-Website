db58eca8c8caf548c1747a4b44c86255d52b21dc
Anish Rajan <56666741+anishrajan25@users.noreply.github.com>
2021-03-28 23:31:19 +0530

    fetched data from server

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,28 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Container, Row, Col } from "reactstrap";
 import ProductCard from "../ProductCard";
 import "./styles.css";
 
 const Home = () => {
+  const [products, setProducts] = useState(null);
+
+  const fetchProductData = async () => {
+    const data = await fetch("https://i8u9f.sse.codesandbox.io/");
+    console.log(await data.json());
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
   return (
     <Container className="my-3">
       <Row>
         <Col className="text-right" xs="12">
-          
-            <Link to="/cart"><Button color="primary">Go To Cart..!</Button></Link>
-          
+          <Link to="/cart">
+            <Button color="primary">Go To Cart..!</Button>
+          </Link>
         </Col>
       </Row>
       <Row className="align-item-center">
